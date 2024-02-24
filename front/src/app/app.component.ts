f149b0adf4d108f7aef053332d8374c1fa593820
久賀　鈴香 <kuga.suzuka@suzukishouten.co.jp>
2024-02-24 19:29:59 +0900

    editorを表示

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { QuillEditorComponent } from 'ngx-quill';
+import { EditorComponent } from './components/editor/editor.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QuillEditorComponent, EditorComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'sample';
-}
+export class AppComponent {}
---
+++ front/src/app/components/editor/editor.component.spec.ts
@@ -0,0 +1,20 @@
+import { TestBed, waitForAsync } from '@angular/core/testing';
+
+import { DefaultComponent } from './editor.component';
+import { QuillModule } from 'ngx-quill';
+
+describe('Default component', () => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [],
+      imports: [DefaultComponent, QuillModule.forRoot()],
+      providers: QuillModule.forRoot().providers,
+    }).compileComponents();
+  }));
+
+  it('test', () => {
+    const fixture = TestBed.createComponent(DefaultComponent);
+
+    expect(fixture).toBeDefined();
+  });
+});
